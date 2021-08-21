57e6d73c7a916277f2c647321e7d3adfbce0b51d
Sharan Rathnakumar <53375728+sharanrathnakumar@users.noreply.github.com>
2021-08-21 21:35:54 +0530

    Create Set.Union()_Operation.py

---
+++ Set.Union()_Operation.py
@@ -0,0 +1,10 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+no_Eng_std=input()
+roll_no_eng=set(map(int,input().split()))
+no_Frn_std=input()
+roll_no_Frn=set(map(int,input().split()))
+
+eng_set=set(roll_no_eng)
+atleast_one=eng_set.union(roll_no_Frn)
+num=len(atleast_one)
+print(num)
